f3b0437dc655da380992aca6f5388bae3c1c5714
BTC001B <btc001b.btcglobal@gmail.com>
2025-09-07 12:30:11 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ,
-    dialect: process.env.DB_DIALECT || 'postgres',
-    logging: false, // set true to see SQL queries
-  }
-);
+// Use Internal URL directly from .env
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false, // set true to see SQL queries
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // needed for Render
+    },
+  },
+});
 
 sequelize.authenticate()
   .then(() => console.log('✅ PostgreSQL connected'))
