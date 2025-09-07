769e04e9d7c8f3e3c32dfb3105eb6af56e0b9879
BTC001B <btc001b.btcglobal@gmail.com>
2025-09-07 12:41:21 +0530

    bb

--- backend/models/Complaint.js
+++ backend/models/Complaint.js
@@ -92,7 +92,7 @@ const Complaint = sequelize.define("Complaint", {
     type: DataTypes.INTEGER,
     allowNull: true,
     references: {
-      model: "Admin",
+      model: "admins",
       key: "id",
     },
   },
